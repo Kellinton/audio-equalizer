943ee04d9917bf51354e66d1e5769a72c83dd3ea
Kellinton <kellintonbrasil@gmail.com>
2025-02-09 22:58:32 -0300

    feat: adiciona equalizador, controles de play/pause e volume

--- src/app.js
+++ src/app.js
@@ -1,8 +1,58 @@
 const audio = document.getElementById("audio");
-const playPauseButton = document.getElementById("playPauseButton");
-const volumeSlider = document.getElementById("volumeSlider");
+const playerContainer = document.querySelector(".player-container");
 
-// Função para alternar entre Play e Pause
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+const analyser = audioContext.createAnalyser();
+const source = audioContext.createMediaElementSource(audio);
+
+source.connect(analyser);
+analyser.connect(audioContext.destination);
+
+analyser.fftSize = 256;
+const bufferLength = analyser.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
+
+function createEqualizer() {
+    const equalizer = document.createElement('div');
+    equalizer.classList.add('equalizer');
+    
+    for (let i = 0; i < 18; i++) {
+        const bar = document.createElement('div');
+        bar.classList.add('bar');
+        bar.id = `bar${i + 1}`; 
+        equalizer.appendChild(bar);
+    }
+
+    playerContainer.insertBefore(equalizer, audio); 
+}
+
+
+function updateEqualizer() {
+    analyser.getByteFrequencyData(dataArray);
+
+    const bars = document.querySelectorAll(".bar");
+
+    for (let i = 0; i < bars.length; i++) {
+        const value = dataArray[i * 2] * 2;  
+        const scale = (value / 250) * 1;  
+
+        bars[i].style.transform = `scaleY(${Math.min(4, Math.max(0.1, scale))})`; 
+
+        const gradient = `linear-gradient(to top, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, ${scale / 1}) 100%)`;
+        bars[i].style.background = gradient;
+    }
+
+    requestAnimationFrame(updateEqualizer);
+}
+
+audio.onplay = () => {
+    audioContext.resume().then(() => {
+        updateEqualizer();
+    });
+};
+
+
+const playPauseButton = document.getElementById("playPauseButton");
 playPauseButton.addEventListener("click", () => {
     if (audio.paused) {
         audio.play();
@@ -13,7 +63,10 @@ playPauseButton.addEventListener("click", () => {
     }
 });
 
-// Controle do volume
-volumeSlider.addEventListener("input", () => {
-    audio.volume = volumeSlider.value;
+const volumeSlider = document.getElementById("volumeSlider");
+volumeSlider.addEventListener("input", (e) => {
+    audio.volume = e.target.value;
 });
+
+
+createEqualizer();
